3641ca8f84665d9f35735186f14ddaa911cea38f
Alaina <alaina.gossett@gmail.com>
2021-10-30 14:32:59 -0600

    cleans up JS and HTML

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 //CONSTANTS
 const BASE_URL = 'https://www.boredapi.com/api/activity';
 // const API_KEY = ''; this API does not require a key
@@ -21,25 +20,24 @@ $form.on('submit', handleSubmit);
 
 //FUNCTIONS
 
-function handleSubmit(evt){
-//function to get data from API
-    console.log(evt);
+function handleSubmit(evt) {
+    //function to get data from API
+    // console.log(evt);
     evt.preventDefault();
-    $.ajax('http://www.boredapi.com/api/activity')
+    $.ajax(BASE_URL)
         .then(function (data) {
             activityData = data;
-            render ();
+            render();
         }, function (error) {
             console.log('error:', error)
-        }
-        )
+        })
 }
 
 function render() {
-//function to add data to DOM content
-    $main.html( `
+    //function to add data to DOM content
+    $main.html(`
         <h3>Activity:<br/> ${activityData.activity}</h3><br/>
         <p><strong>Type:</strong><br/> ${activityData.type}</p><br/>
         <p><strong>Participants:</strong><br/> ${activityData.participants}</p>
-    `)    
+    `);
 }
